417bfa212718748c34999efb7d0e7380f3c3fbbf
Yashikpk2 <padiyamandayashik@gmail.com>
2021-03-23 09:31:19 +0530

    Create Switch_internalpullup.ino
    Using of switch with internal pullup resistor

---
+++ Switch_internalpullup.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7,INPUT_PULLUP);
+  pinMode(10,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(7)==LOW)
+  {
+    digitalWrite(10,HIGH);
+    delay(500);
+    digitalWrite(10,LOW);
+    delay(500);
+  }
+
+}
