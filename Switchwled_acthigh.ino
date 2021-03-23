713ecadef29b4fe763d7cc78bc11605d12f448cc
Yashikpk2 <padiyamandayashik@gmail.com>
2021-03-23 10:52:04 +0530

    Switch with led at active high
    Configure switch led in arduino using active high

---
+++ Switchwled_acthigh.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7,INPUT);
+  pinMode(10,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(7)==HIGH)
+  {
+    digitalWrite(10,HIGH);
+    delay(250);
+    digitalWrite(10,LOW);
+    delay(250);
+    digitalWrite(10,HIGH);
+    delay(250);
+    digitalWrite(10,LOW);
+    delay(250);
+  }
+
+}
